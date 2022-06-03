ba9c71ef2cf4106a2eda1fee39e0c62bf494b4d9
xlExistence <102565353+xlExistence@users.noreply.github.com>
2022-06-03 17:44:43 -0500

    Para poder hacer pull

--- index.php
+++ index.php
@@ -7,10 +7,8 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Recetario | Inicio</title>
-    <link rel="stylesheet" href="CSS\index_css.css">
+    <link rel="stylesheet" href="CSS\index__css.css">
 </head>
 <body>
     

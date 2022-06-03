d02c54bef08747b69800134ae4c373535bfee89a
AlC0DEMAGO <cesaralatorre22@gmail.com>
2022-06-03 17:53:25 -0500

    si

--- cambios.php
+++ cambios.php
@@ -20,7 +20,7 @@
 <body>
     <div class="container">
         <img id="img_bann" src="https://images.squarespace-cdn.com/content/v1/52c6f529e4b001bd32397de6/1436183811286-C375KU5TBN5C4A0QT91D/image-asset.jpeg?format=1000w">
-asdasdasdad
+
     </div>
 
     <div class="container">

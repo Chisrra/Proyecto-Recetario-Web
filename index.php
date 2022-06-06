32085e1ed87489a57a2e9c391c726cf1a991005e
xlExistence <102565353+xlExistence@users.noreply.github.com>
2022-06-06 13:50:52 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
     require_once "conexion.php";
-    require_once "menu-desplegable.php";
 ?>
 
 <!DOCTYPE html>
@@ -12,6 +11,10 @@
     <link rel="stylesheet" href="CSS\index_css.css">
 </head>
 <body>
+    <?php
+        require_once "menu-desplegable.php";
+    ?>
+
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
         <path fill="#ffffff4d" fill-opacity="1" d="M0,288L60,277.3C120,267,240,245,360,234.7C480,224,600,224,720,234.7C840,245,960,267,1080,266.7C1200,267,1320,245,1380,234.7L1440,224L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path>
     </svg>

0de333627467de1d92704ac90ac9f068c45def03
xlExistence <102565353+xlExistence@users.noreply.github.com>
2022-06-06 13:49:07 -0500

    Eliminar DOCTYPE del menú desplegable

--- menu-desplegable.php
+++ menu-desplegable.php
@@ -1,15 +1,16 @@
 <?php
 ?>
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="es">
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         
-        <link rel="stylesheet" href="CSS\menu-desplegable_css.css">
+        
     </head>
-    <body>
+    <body> -->
+        <link rel="stylesheet" href="CSS\menu-desplegable_css.css">
         <div class="btn-menu">
 			<label for="btn-menu">☰</label>
 		</div>
@@ -30,5 +31,5 @@
                 <label for="btn-menu">❌</label>
             </div>
         </div>
-    </body>
-</html>
+    <!-- </body>
+</html> -->

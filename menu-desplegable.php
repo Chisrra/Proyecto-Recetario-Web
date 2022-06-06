dbd298af60f875252d49b93b62d26350af94992b
xlExistence <102565353+xlExistence@users.noreply.github.com>
2022-06-06 15:36:40 -0500

    Comentar HTML innecesario de Menú desplegable

--- menu-desplegable.php
+++ menu-desplegable.php
@@ -1,15 +1,14 @@
-<?php
-?>
-<!DOCTYPE html>
+<?php ?>
+<!-- <!DOCTYPE html>
 <html lang="es">
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="icon" href="IMG/logo.ico">
+        <link rel="icon" href="IMG/logo.ico"> -->
         <link rel="stylesheet" href="CSS\menu-desplegable_css.css">
-    </head>
-    <body>
+    <!-- </head>
+    <body> -->
         <div class="btn-menu">
 			<label for="btn-menu">☰</label>
 		</div>
@@ -30,5 +29,5 @@
                 <label for="btn-menu">❌</label>
             </div>
         </div>
-    </body>
-</html>
+    <!-- </body>
+</html> -->

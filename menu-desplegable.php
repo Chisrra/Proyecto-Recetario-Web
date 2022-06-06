2c0f4cf92bf1caf0d36aaa13687396d9864e185d
Chisrra <cristianisrael2002@hotmail.com>
2022-06-06 16:04:28 -0500

    Integracion adecuada del menu desplegable
    Se elimino parte inecesaria del codigo y que podría causar conflictos.
    
    Co-Authored-By: Josué Polina <102565353+xlExistence@users.noreply.github.com>

--- menu-desplegable.php
+++ menu-desplegable.php
@@ -1,33 +1,24 @@
 <?php ?>
-<!-- <!DOCTYPE html>
-<html lang="es">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="icon" href="IMG/logo.ico"> -->
-        <link rel="stylesheet" href="CSS\menu-desplegable_css.css">
-    <!-- </head>
-    <body> -->
-        <div class="btn-menu">
-			<label for="btn-menu">☰</label>
-		</div>
 
-        <input type="checkbox" id="btn-menu">
-        <div class="container-menu">
-            <div class="cont-menu">
-                <nav>
-                    <a href="index.php" id="logo" title="Inicio">
-                        <img src="IMG\logo.png" alt="logo.png">
-                        <span>Recetado</span>
-                    </a>
-                    <a href="#">Crear receta</a>
-                    <a href="#">Mis recetas</a>
-                    <a href="#">Explorar más recetas</a>
-                    <a href="#" id="close">Cerrar sesión</a>
-                </nav>
-                <label for="btn-menu">❌</label>
-            </div>
-        </div>
-    <!-- </body>
-</html> -->
+<!-- Inicio del Menu desplegable -->
+<div class="btn-menu">
+    <label for="btn-menu">☰</label>
+</div>
+
+<input type="checkbox" id="btn-menu">
+<div class="container-menu">
+    <div class="cont-menu">
+        <nav>
+            <a href="index.php" id="logo" title="Inicio">
+                <img src="IMG\logo.png" alt="logo.png">
+                <span>Recetado</span>
+            </a>
+            <a href="#">Crear receta</a>
+            <a href="#">Mis recetas</a>
+            <a href="#">Explorar más recetas</a>
+            <a href="#" id="close">Cerrar sesión</a>
+        </nav>
+        <label for="btn-menu">❌</label>
+    </div>
+</div>
+<!-- Fin del menu desplegable -->

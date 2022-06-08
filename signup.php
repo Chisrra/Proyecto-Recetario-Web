936578258bd8f2d2f6c3164aa0e125466bdb0c36
Chisrra <cristianisrael2002@hotmail.com>
2022-06-07 21:54:21 -0500

    Ajustes del documento

--- signup.php
+++ signup.php
@@ -1,6 +1,6 @@
 <!-- DOC HTML -->
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
     <meta charset="UTF-8">
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="IMG/logo.ico">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <link rel="stylesheet"  href="CSS\singUp.css">
+    <link rel="stylesheet" href="CSS\singUp.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
     <title>Crea tu recetario</title>
 </head>
@@ -43,12 +43,8 @@
 </body>
 
 </html>
-
+<!-- Fin del documento HTML -->
 <?php
-function hola()
-{
-    echo "<p>hola</p>";
-}
 if (isset($_POST['Registro'])) {
     //---Validar
     $valido = true;
@@ -78,26 +74,28 @@ if (isset($_POST['Registro'])) {
     if ($valido) {
         $password = $_POST['passwordUser'];
         $password_hash = password_hash($password, PASSWORD_BCRYPT);
+        $bits = strlen($password_hash);
         //Poner imagen default
         $tipoArchivo = 'image/jpg';
         $nombreArchivo = 'default_user.jpg';
         $sizeArchivo = filesize("IMG\default_user.jpg");
         $imagenDefault = fopen('IMG\default_user.jpg', 'r');
         $imagenDefaultBinaria = fread($imagenDefault, $sizeArchivo);
-        $imagenDefaultBinaria = mysqli_escape_string($conn,$imagenDefaultBinaria);
+        $imagenDefaultBinaria = mysqli_escape_string($conn, $imagenDefaultBinaria);
         //Fin de imagen default
+
         $query = "INSERT INTO `usuario`(`Correo`, `Contraseña`, `NombreUsuario`, `NombreImagen`, `ImagenPerfil`, `Tipo`) VALUES ('$email','$password_hash','$nombreUsuario','default_user.jpg','$imagenDefaultBinaria','image/jpg')";
         if (mysqli_query($conn, $query)) {
             $query = "SELECT `idUsuario` FROM `usuario` WHERE `NombreUsuario` = '$nombreUsuario'";
-            if($rs = mysqli_query($conn,$query)){
+            if ($rs = mysqli_query($conn, $query)) {
                 $fila = mysqli_fetch_array($rs);
                 $_SESSION['idUsuario'] = $fila['idUsuario'];
                 echo '<script>
-                alert("¡Se ha registrado con exito! :)");
-                // location.href="../Proyecto-Recetario-Web";
+                    window.location.href= "../Proyecto-Recetario-Web";
+                    alert("¡Registro exitoso!");
                 </script>';
                 mysqli_close($conn);
-                header("Location: ../Proyecto-Recetario-Web",TRUE,301);
+                // header("Location: ../Proyecto-Recetario-Web", TRUE, 301);
                 exit();
             }
         } else {

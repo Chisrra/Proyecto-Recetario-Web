1570181669f3773c59674ad70425dff4e6ac1a81
Chisrra <cristianisrael2002@hotmail.com>
2022-06-09 01:28:49 -0500

    JQuery y funcion showPassword

--- login.php
+++ login.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" type="text/css" href="CSS/logIn.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <script src="JS/jquery.js" type="text/javascript"></script>
     <title>Tu recetario Web</title>
 </head>
 
@@ -36,6 +37,27 @@
             <button class="btn btn-secondary" type="button" style="float: right;" onclick="location.href='signup.php'">Registrarse :D</button>
         </form>
     </div>
+
+    <!-- jquery  -->
+    <script type="text/javascript">
+        $(document).ready(function(){
+
+            // Mostrar contaseña
+            if( $('#showC').on('change', function(){
+                if( $('#showC').is(':checked')){
+                    $('#passwordUser').removeAttr('type');
+                }else{
+                    $('#passwordUser').attr('type','password');
+                }
+            }));
+
+            // Fin mostrar contaseña
+            
+        });
+
+    </script>
+    <!-- jquery  -->
+
 </body>
 
 </html>

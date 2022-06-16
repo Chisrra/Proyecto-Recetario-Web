67257e67c3f384fbc71080f79b2f9a5aff2c36c8
xlExistence <102565353+xlExistence@users.noreply.github.com>
2022-06-16 16:54:33 -0500

    Ajuste de btn hamburguesa y cambios en muestra de recetas

--- PHP_index/AñadirMisRecetas.php
+++ PHP_index/AñadirMisRecetas.php
@@ -6,28 +6,27 @@ if ($result = mysqli_query($conn, $query)) {
     $row = mysqli_num_rows($result);
     if ($row) {
         while ($fil = $result->fetch_assoc()) {
-            // ?nombre=Chisrra&prim=5
-            echo "<a href='receta.php?id={$fil['id']}'>";
             if (++$fila == $row) echo ("<div class='receta last' >");
             else {
                 if ($fila == 1) echo ('<div class="receta first sup">');
                 else echo ('<div class="receta sup">');
             }
 
-            if ($fila == 1) echo ('<div class="imagen_r first_i">');
-            else if ($fila == $row) echo ('<div class="imagen_r last_i">');
-            else echo ('<div class="imagen_r">');
-            echo ('         <img src="data:;base64,' . base64_encode($fil["Foto_Platillo"]) . '" alt="receta' . $row . '.jpg">
-                            </div>
-                            <div class="texto">
+            if ($fila == 1) echo ('<a href="receta.php?id='.$fil["id"].'" class="imagen_r first_i">');
+            else if ($fila == $row) echo ('<a href="receta.php?id='.$fil["id"].'" class="imagen_r last_i">');
+            else echo ('<a href="receta.php?id='.$fil["id"].'" class="imagen_r">');
+            echo ('             <img src="data:;base64,' . base64_encode($fil["Foto_Platillo"]) . '" alt="receta' . $row . '.jpg">
+                            </a>
+                            
+                            <a href="receta.php?id='.$fil["id"].'" class="texto">
                                 <span class="titulo">' . $fil["Nombre_Receta"] . '</span>
                                 <span class="descripcion">' . $fil["Descripcion_Breve"] . '</span>
-                            </div>
+                            </a>
                             <div class="btns">
                                 <button type="submit" class="btn modif" title="Modificar receta">' . $fil["id"] . '</button>
                                 <button type="submit" class="btn elim" title="Eliminar receta">' . $fil["id"] . '</button>
                             </div>
-                        </div></a>');
+                        </div>');
         }
     } else {
         echo ('<p id="empty">No tienes recetas, <a href="altas.php">¡ crea una !</a></p>');

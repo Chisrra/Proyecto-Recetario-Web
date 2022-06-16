e1c1bc7dc519a7e946d27abd06f95958b06815b5
Chisrra <cristianisrael2002@hotmail.com>
2022-06-16 15:00:22 -0500

    Formato y etiqueta <a> para $_GET

--- PHP_index/AñadirMisRecetas.php
+++ PHP_index/AñadirMisRecetas.php
@@ -1,35 +1,35 @@
 <?php
-    $fila = 0;
+$fila = 0;
 
-    $query = "SELECT * FROM receta WHERE Usuario_idUsuario = {$_SESSION['idUsuario']}";
-    if ($result = mysqli_query($conn, $query)) {
-        $row = mysqli_num_rows($result);
-        if ($row) {
-            while($fil = $result->fetch_assoc()) {
-                if (++$fila == $row) echo ('<div class="receta last">');
-                else {
-                    if ($fila == 1) echo('<div class="receta first sup">');
-                    else echo('<div class="receta sup">');
-                }
+$query = "SELECT * FROM receta WHERE Usuario_idUsuario = {$_SESSION['idUsuario']}";
+if ($result = mysqli_query($conn, $query)) {
+    $row = mysqli_num_rows($result);
+    if ($row) {
+        while ($fil = $result->fetch_assoc()) {
+            // ?nombre=Chisrra&prim=5
+            echo "<a href='receta.php?id={$fil['id']}'>";
+            if (++$fila == $row) echo ("<div class='receta last' >");
+            else {
+                if ($fila == 1) echo ('<div class="receta first sup">');
+                else echo ('<div class="receta sup">');
+            }
 
-                if ($fila == 1) echo('<div class="imagen_r first_i">');
-                else if ($fila == $row) echo('<div class="imagen_r last_i">');
-                else echo('<div class="imagen_r">');
-                echo ('         <img src="data:;base64,'.base64_encode($fil["Foto_Platillo"]).'" alt="receta'.$row.'.jpg">
+            if ($fila == 1) echo ('<div class="imagen_r first_i">');
+            else if ($fila == $row) echo ('<div class="imagen_r last_i">');
+            else echo ('<div class="imagen_r">');
+            echo ('         <img src="data:;base64,' . base64_encode($fil["Foto_Platillo"]) . '" alt="receta' . $row . '.jpg">
                             </div>
                             <div class="texto">
-                                <span class="titulo">'.$fil["Nombre_Receta"].'</span>
-                                <span class="descripcion">'.$fil["Descripcion_Breve"].'</span>
+                                <span class="titulo">' . $fil["Nombre_Receta"] . '</span>
+                                <span class="descripcion">' . $fil["Descripcion_Breve"] . '</span>
                             </div>
                             <div class="btns">
-                                <button type="submit" class="btn modif" title="Modificar receta">'.$fil["id"].'</button>
-                                <button type="submit" class="btn elim" title="Eliminar receta">'.$fil["id"].'</button>
+                                <button type="submit" class="btn modif" title="Modificar receta">' . $fil["id"] . '</button>
+                                <button type="submit" class="btn elim" title="Eliminar receta">' . $fil["id"] . '</button>
                             </div>
-                        </div>');
-            }
+                        </div></a>');
         }
-        else {
-            echo ('<p id="empty">No tienes recetas, <a href="altas.php">¡ crea una !</a></p>');
-        }
+    } else {
+        echo ('<p id="empty">No tienes recetas, <a href="altas.php">¡ crea una !</a></p>');
     }
-?>
+}

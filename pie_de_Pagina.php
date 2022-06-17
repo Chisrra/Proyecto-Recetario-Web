0a99ddb4f5376ea562e073efb95cf9fdddff72cf
Chisrra <cristianisrael2002@hotmail.com>
2022-06-16 23:51:59 -0500

    Pie de pagina del sitio Web

---
+++ pie_de_Pagina.php
@@ -0,0 +1,63 @@
+<footer class="bg-dark text-center text-white">
+  <!-- 1erDiv -->
+  <div class="container p-4">
+    <!-- Social media -->
+    <section class="mb-4">
+      <a class="btn btn-outline-light btn-floating m-1" href="https://github.com/Chisrra" role="button" target="_blank" rel="noopener noreferrer"
+        ><i class="fab fa-facebook-f">Ing.Donato</i
+      ></a>
+
+      <a class="btn btn-outline-light btn-floating m-1" href="https://github.com/xlExistence" role="button" target="_blank" rel="noopener noreferrer"
+        ><i class="fab fa-twitter">Ing.Polina</i
+      ></a>
+
+      <a class="btn btn-outline-light btn-floating m-1" href="https://github.com/AlC0DEMAGO" role="button" target="_blank" rel="noopener noreferrer"
+        ><i class="fab fa-google">Ing.Alatorre</i
+      ></a>
+
+      <a class="btn btn-outline-light btn-floating m-1" href="https://github.com/EduardoGooj" role="button" target="_blank" rel="noopener noreferrer"
+        ><i class="fab fa-instagram">Ing.Ojeda</i
+      ></a>
+    </section>
+    <!-- Fin Social media -->
+
+    <!-- Texto -->
+    <section class="mb-4">
+      <p>
+        Este trabajo fue hecho con cariño y dedicación para la materia de Lenguajes de Computación IV
+      </p>
+    </section>
+    <!-- Fin Texto -->
+
+    <!-- Links -->
+    <section class="">
+      <!--Div row-->
+      <div class="row align-items-center">
+        <!--Div column-->
+        <div class="col align-self-center">
+          <h5 class="text-uppercase">Agradecimientos</h5>
+
+          <ul class="list-unstyled mb-0">
+            <li>
+              <a href="https://www.uaa.mx/portal/" class="text-white">Universidad Autonoma de Aguascalientes</a>
+            </li>
+            <li>
+              <a href="https://stackoverflow.com" class="text-white">stackoverflow</a>
+            </li>
+          </ul>
+        </div>
+        <!--Fin Div column-->
+      </div>
+      <!--Fin Div row-->
+    </section>
+    <!-- Fin Links -->
+  </div>
+  <!-- Fin 1erDiv -->
+
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background: #192120;">
+    © 2022 Copyright:
+    <p class="text-white">RowWhats</p>
+  </div>
+  <!-- Copyright -->
+</footer>
